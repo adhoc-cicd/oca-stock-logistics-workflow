OCA/stock-logistics-workflow

[FIX] stock_split_picking: 'done' mode
This commit fixes the handling of the split with 'Done quantities'
for cases where there is a move with no qty_done such as below.

PICK_1
       product_uom_qty  qty_done
MOVE_1        3            0
MOVE_2        3            1

Before this commit, the result of splitting with mode
'Done quantities' would be as follows:

PICK_1
       product_uom_qty  qty_done
MOVE_1        3            0
MOVE_2        1            1

PICK_2 (newly created)
       product_uom_qty  qty_done
MOVE_3        2            0      <- split from MOVE_2

With ithis commit the result will be as follows:

PICK_1
       product_uom_qty  qty_done
MOVE_2        1            1

PICK_2 (newly created)
       product_uom_qty  qty_done
MOVE_1        3            0      <- moved from PICK_1
MOVE_3        2            0      <- split from MOVE_2

stock_split_picking/models/stock_picking.py
@@ -41,16 +41,23 @@ class StockPicking(models.Model):
                     qty_uom_split = move.product_uom._compute_quantity(
                         qty_split, move.product_id.uom_id, rounding_method="HALF-UP"
                     )
+                    # Empty list is returned for moves with zero qty_done.
                     new_move_vals = move._split(qty_uom_split)
-                    for move_line in move.move_line_ids:
-                        if move_line.product_qty and move_line.qty_done:
-                            # To avoid an error
-                            # when picking is partially available
-                            try:
-                                move_line.write({"product_uom_qty": move_line.qty_done})
-                            except UserError:
-                                continue
-                    new_move = self.env["stock.move"].create(new_move_vals)
+                    if new_move_vals:
+                        for move_line in move.move_line_ids:
+                            if move_line.product_qty and move_line.qty_done:
+                                # To avoid an error
+                                # when picking is partially available
+                                try:
+                                    move_line.write(
+                                        {"product_uom_qty": move_line.qty_done}
+                                    )
+                                except UserError:
+                                    continue
+                        new_move = self.env["stock.move"].create(new_move_vals)
+                    # Moves with no qty_done should be moved to the new picking.
+                    else:
+                        new_move = move
                     new_move._action_confirm(merge=False)
                     new_moves |= new_move
 

stock_split_picking/tests/test_stock_split_picking.py
@@ -14,6 +14,7 @@ class TestStockSplitPicking(TransactionCase):
         cls.src_location = cls.env.ref("stock.stock_location_stock")
         cls.dest_location = cls.env.ref("stock.stock_location_customers")
         cls.product = cls.env["product.product"].create({"name": "Test product"})
+        cls.product_2 = cls.env["product.product"].create({"name": "Test product 2"})
         cls.partner = cls.env["res.partner"].create({"name": "Test partner"})
         cls.picking = cls.env["stock.picking"].create(
             {
@@ -23,17 +24,22 @@ class TestStockSplitPicking(TransactionCase):
                 "location_dest_id": cls.dest_location.id,
             }
         )
-        cls.move = cls.env["stock.move"].create(
-            {
-                "name": "/",
-                "picking_id": cls.picking.id,
-                "product_id": cls.product.id,
-                "product_uom_qty": 10,
-                "product_uom": cls.product.uom_id.id,
-                "location_id": cls.src_location.id,
-                "location_dest_id": cls.dest_location.id,
-            }
-        )
+
+        def _create_stock_move(product):
+            return cls.env["stock.move"].create(
+                {
+                    "name": "/",
+                    "picking_id": cls.picking.id,
+                    "product_id": product.id,
+                    "product_uom_qty": 10,
+                    "product_uom": product.uom_id.id,
+                    "location_id": cls.src_location.id,
+                    "location_dest_id": cls.dest_location.id,
+                }
+            )
+
+        cls.move = _create_stock_move(cls.product)
+        cls.move_2 = _create_stock_move(cls.product_2)
 
     def test_stock_split_picking(self):
         # Picking state is draft
@@ -49,7 +55,18 @@ class TestStockSplitPicking(TransactionCase):
         # We assign quantities in order to split
         self.picking.action_assign()
         move_line = self.env["stock.move.line"].search(
-            [("picking_id", "=", self.picking.id)], limit=1
+            [
+                ("picking_id", "=", self.picking.id),
+                ("product_id", "=", self.product.id),
+            ],
+            limit=1,
+        )
+        move_line_2 = self.env["stock.move.line"].search(
+            [
+                ("picking_id", "=", self.picking.id),
+                ("product_id", "=", self.product_2.id),
+            ],
+            limit=1,
         )
         move_line.qty_done = 4.0
         # Split picking: 4 and 6
@@ -65,22 +82,54 @@ class TestStockSplitPicking(TransactionCase):
         self.assertAlmostEqual(self.move.product_qty, 4.0)
         self.assertAlmostEqual(self.move.product_uom_qty, 4.0)
 
+        self.assertEqual(move_line.picking_id, self.picking)
+        self.assertEqual(self.move.picking_id, self.picking)
+        # move/move_line with no done qty no longer belongs to the original picking.
+        self.assertNotEqual(move_line_2.picking_id, self.picking)
+        self.assertNotEqual(self.move_2.picking_id, self.picking)
+
         self.assertEqual(self.picking.state, "assigned")
         # An another one with 6 units in state assigned
         new_picking = self.env["stock.picking"].search(
             [("backorder_id", "=", self.picking.id)], limit=1
         )
         move_line = self.env["stock.move.line"].search(
-            [("picking_id", "=", new_picking.id)], limit=1
+            [("picking_id", "=", new_picking.id), ("product_id", "=", self.product.id)],
+            limit=1,
+        )
+        move_line_2 = self.env["stock.move.line"].search(
+            [
+                ("picking_id", "=", new_picking.id),
+                ("product_id", "=", self.product_2.id),
+            ],
+            limit=1,
         )
 
         self.assertAlmostEqual(move_line.qty_done, 0.0)
         self.assertAlmostEqual(move_line.product_qty, 6.0)
         self.assertAlmostEqual(move_line.product_uom_qty, 6.0)
+        self.assertAlmostEqual(move_line_2.qty_done, 0.0)
+        self.assertAlmostEqual(move_line_2.product_qty, 10.0)
+        self.assertAlmostEqual(move_line_2.product_uom_qty, 10.0)
 
-        self.assertAlmostEqual(new_picking.move_lines.quantity_done, 0.0)
-        self.assertAlmostEqual(new_picking.move_lines.product_qty, 6.0)
-        self.assertAlmostEqual(new_picking.move_lines.product_uom_qty, 6.0)
+        move = self.env["stock.move"].search(
+            [("picking_id", "=", new_picking.id), ("product_id", "=", self.product.id)],
+            limit=1,
+        )
+        move_2 = self.env["stock.move"].search(
+            [
+                ("picking_id", "=", new_picking.id),
+                ("product_id", "=", self.product_2.id),
+            ],
+            limit=1,
+        )
+
+        self.assertAlmostEqual(move.quantity_done, 0.0)
+        self.assertAlmostEqual(move.product_qty, 6.0)
+        self.assertAlmostEqual(move.product_uom_qty, 6.0)
+        self.assertAlmostEqual(move_2.quantity_done, 0.0)
+        self.assertAlmostEqual(move_2.product_qty, 10.0)
+        self.assertAlmostEqual(move_2.product_uom_qty, 10.0)
 
         self.assertEqual(new_picking.state, "assigned")
 

adhoc-cicd/oca-stock-logistics-workflow

stock_split_picking/models/stock_picking.py
@@ -43,18 +43,22 @@ class StockPicking(models.Model):
                     qty_uom_split = move.product_uom._compute_quantity(
                         qty_split, move.product_id.uom_id, rounding_method="HALF-UP"
                     )
+                    # Empty list is returned for moves with zero qty_done.
                     new_move_vals = move._split(qty_uom_split)
-                    for move_line in move.move_line_ids:
-                        if move_line.reserved_qty and move_line.qty_done:
-                            # To avoid an error
-                            # when picking is partially available
-                            try:
-                                move_line.write(
-                                    {"reserved_uom_qty": move_line.qty_done}
-                                )
-                            except UserError:
-                                continue
-                    new_move = self.env["stock.move"].create(new_move_vals)
+                    if new_move_vals:
+                        for move_line in move.move_line_ids:
+                            if move_line.reserved_qty and move_line.qty_done:
+                                # To avoid an error
+                                # when picking is partially available
+                                try:
+                                    move_line.write(
+                                        {"reserved_uom_qty": move_line.qty_done}
+                                    )
+                                except UserError:
+                                    continue
+                        new_move = self.env["stock.move"].create(new_move_vals)
+                    else:
+                        new_move = move
                     new_move._action_confirm(merge=False)
                     new_moves |= new_move
 

stock_split_picking/tests/test_stock_split_picking.py
@@ -14,6 +14,7 @@ class TestStockSplitPicking(TransactionCase):
         cls.src_location = cls.env.ref("stock.stock_location_stock")
         cls.dest_location = cls.env.ref("stock.stock_location_customers")
         cls.product = cls.env["product.product"].create({"name": "Test product"})
+        cls.product_2 = cls.env["product.product"].create({"name": "Test product 2"})
         cls.partner = cls.env["res.partner"].create({"name": "Test partner"})
         cls.picking = cls.env["stock.picking"].create(
             {
@@ -23,17 +24,22 @@ class TestStockSplitPicking(TransactionCase):
                 "location_dest_id": cls.dest_location.id,
             }
         )
-        cls.move = cls.env["stock.move"].create(
-            {
-                "name": "/",
-                "picking_id": cls.picking.id,
-                "product_id": cls.product.id,
-                "product_uom_qty": 10,
-                "product_uom": cls.product.uom_id.id,
-                "location_id": cls.src_location.id,
-                "location_dest_id": cls.dest_location.id,
-            }
-        )
+
+        def _create_stock_move(product):
+            return cls.env["stock.move"].create(
+                {
+                    "name": "/",
+                    "picking_id": cls.picking.id,
+                    "product_id": product.id,
+                    "product_uom_qty": 10,
+                    "product_uom": product.uom_id.id,
+                    "location_id": cls.src_location.id,
+                    "location_dest_id": cls.dest_location.id,
+                }
+            )
+
+        cls.move = _create_stock_move(cls.product)
+        cls.move_2 = _create_stock_move(cls.product_2)
 
     def test_stock_split_picking(self):
         # Picking state is draft
@@ -49,7 +55,18 @@ class TestStockSplitPicking(TransactionCase):
         # We assign quantities in order to split
         self.picking.action_assign()
         move_line = self.env["stock.move.line"].search(
-            [("picking_id", "=", self.picking.id)], limit=1
+            [
+                ("picking_id", "=", self.picking.id),
+                ("product_id", "=", self.product.id),
+            ],
+            limit=1,
+        )
+        move_line_2 = self.env["stock.move.line"].search(
+            [
+                ("picking_id", "=", self.picking.id),
+                ("product_id", "=", self.product_2.id),
+            ],
+            limit=1,
         )
         move_line.qty_done = 4.0
         # Split picking: 4 and 6
@@ -65,22 +82,54 @@ class TestStockSplitPicking(TransactionCase):
         self.assertAlmostEqual(self.move.product_qty, 4.0)
         self.assertAlmostEqual(self.move.product_uom_qty, 4.0)
 
+        self.assertEqual(move_line.picking_id, self.picking)
+        self.assertEqual(self.move.picking_id, self.picking)
+        # move/move_line with no done qty no longer belongs to the original picking.
+        self.assertNotEqual(move_line_2.picking_id, self.picking)
+        self.assertNotEqual(self.move_2.picking_id, self.picking)
+
         self.assertEqual(self.picking.state, "assigned")
         # An another one with 6 units in state assigned
         new_picking = self.env["stock.picking"].search(
             [("backorder_id", "=", self.picking.id)], limit=1
         )
         move_line = self.env["stock.move.line"].search(
-            [("picking_id", "=", new_picking.id)], limit=1
+            [("picking_id", "=", new_picking.id), ("product_id", "=", self.product.id)],
+            limit=1,
+        )
+        move_line_2 = self.env["stock.move.line"].search(
+            [
+                ("picking_id", "=", new_picking.id),
+                ("product_id", "=", self.product_2.id),
+            ],
+            limit=1,
         )
 
         self.assertAlmostEqual(move_line.qty_done, 0.0)
         self.assertAlmostEqual(move_line.reserved_qty, 6.0)
         self.assertAlmostEqual(move_line.reserved_uom_qty, 6.0)
+        self.assertAlmostEqual(move_line_2.qty_done, 0.0)
+        self.assertAlmostEqual(move_line_2.reserved_qty, 10.0)
+        self.assertAlmostEqual(move_line_2.reserved_uom_qty, 10.0)
 
-        self.assertAlmostEqual(new_picking.move_ids.quantity_done, 0.0)
-        self.assertAlmostEqual(new_picking.move_ids.product_qty, 6.0)
-        self.assertAlmostEqual(new_picking.move_ids.product_uom_qty, 6.0)
+        move = self.env["stock.move"].search(
+            [("picking_id", "=", new_picking.id), ("product_id", "=", self.product.id)],
+            limit=1,
+        )
+        move_2 = self.env["stock.move"].search(
+            [
+                ("picking_id", "=", new_picking.id),
+                ("product_id", "=", self.product_2.id),
+            ],
+            limit=1,
+        )
+
+        self.assertAlmostEqual(move.quantity_done, 0.0)
+        self.assertAlmostEqual(move.product_qty, 6.0)
+        self.assertAlmostEqual(move.product_uom_qty, 6.0)
+        self.assertAlmostEqual(move_2.quantity_done, 0.0)
+        self.assertAlmostEqual(move_2.product_qty, 10.0)
+        self.assertAlmostEqual(move_2.product_uom_qty, 10.0)
 
         self.assertEqual(new_picking.state, "assigned")
 