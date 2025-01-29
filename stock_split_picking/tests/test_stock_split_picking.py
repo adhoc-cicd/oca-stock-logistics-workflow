camptocamp/stock-logistics-workflow

[MIG][11.0] stock_split_picking

stock_split_picking/tests/test_stock_split_picking.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Vicent Cubells <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -49,20 +48,39 @@ class TestStockSplitPicking(SavepointCase):
             self.picking.split_process()
         # We assign quantities in order to split
         self.picking.action_assign()
-        pack_opt = self.env['stock.pack.operation'].search(
+        pack_opt = self.env['stock.move.line'].search(
             [('picking_id', '=', self.picking.id)], limit=1)
         pack_opt.qty_done = 4.0
         # Split picking: 4 and 6
+        # import pdb; pdb.set_trace()
         self.picking.split_process()
+
         # We have a picking with 4 units in state assigned
         self.assertAlmostEqual(pack_opt.qty_done, 4.0)
         self.assertAlmostEqual(pack_opt.product_qty, 4.0)
+        self.assertAlmostEqual(pack_opt.product_uom_qty, 4.0)
+        self.assertAlmostEqual(pack_opt.ordered_qty, 10.0)
+
+        self.assertAlmostEqual(self.move.quantity_done, 4.0)
+        self.assertAlmostEqual(self.move.product_qty, 4.0)
+        self.assertAlmostEqual(self.move.product_uom_qty, 4.0)
+        self.assertAlmostEqual(self.move.ordered_qty, 10.0)
+
         self.assertEqual(self.picking.state, 'assigned')
         # An another one with 6 units in state assigned
         new_picking = self.env['stock.picking'].search(
             [('backorder_id', '=', self.picking.id)], limit=1)
-        pack_opt = self.env['stock.pack.operation'].search(
+        pack_opt = self.env['stock.move.line'].search(
             [('picking_id', '=', new_picking.id)], limit=1)
+
         self.assertAlmostEqual(pack_opt.qty_done, 0.0)
         self.assertAlmostEqual(pack_opt.product_qty, 6.0)
+        self.assertAlmostEqual(pack_opt.product_uom_qty, 6.0)
+        self.assertAlmostEqual(pack_opt.ordered_qty, 6.0)
+
+        self.assertAlmostEqual(new_picking.move_lines.quantity_done, 0.0)
+        self.assertAlmostEqual(new_picking.move_lines.product_qty, 6.0)
+        self.assertAlmostEqual(new_picking.move_lines.product_uom_qty, 6.0)
+        self.assertAlmostEqual(new_picking.move_lines.ordered_qty, 6.0)
+
         self.assertEqual(new_picking.state, 'assigned')

adhoc-cicd/oca-stock-logistics-workflow

stock_split_picking/tests/test_stock_split_picking.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Vicent Cubells <vicent.cubells@tecnativa.com>
+# Copyright 2018 Camptocamp SA - Julien Coux
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import SavepointCase
@@ -49,20 +49,39 @@ class TestStockSplitPicking(SavepointCase):
             self.picking.split_process()
         # We assign quantities in order to split
         self.picking.action_assign()
-        pack_opt = self.env['stock.pack.operation'].search(
+        move_line = self.env['stock.move.line'].search(
             [('picking_id', '=', self.picking.id)], limit=1)
-        pack_opt.qty_done = 4.0
+        move_line.qty_done = 4.0
         # Split picking: 4 and 6
+        # import pdb; pdb.set_trace()
         self.picking.split_process()
+
         # We have a picking with 4 units in state assigned
-        self.assertAlmostEqual(pack_opt.qty_done, 4.0)
-        self.assertAlmostEqual(pack_opt.product_qty, 4.0)
+        self.assertAlmostEqual(move_line.qty_done, 4.0)
+        self.assertAlmostEqual(move_line.product_qty, 4.0)
+        self.assertAlmostEqual(move_line.product_uom_qty, 4.0)
+        self.assertAlmostEqual(move_line.ordered_qty, 10.0)
+
+        self.assertAlmostEqual(self.move.quantity_done, 4.0)
+        self.assertAlmostEqual(self.move.product_qty, 4.0)
+        self.assertAlmostEqual(self.move.product_uom_qty, 4.0)
+        self.assertAlmostEqual(self.move.ordered_qty, 10.0)
+
         self.assertEqual(self.picking.state, 'assigned')
         # An another one with 6 units in state assigned
         new_picking = self.env['stock.picking'].search(
             [('backorder_id', '=', self.picking.id)], limit=1)
-        pack_opt = self.env['stock.pack.operation'].search(
+        move_line = self.env['stock.move.line'].search(
             [('picking_id', '=', new_picking.id)], limit=1)
-        self.assertAlmostEqual(pack_opt.qty_done, 0.0)
-        self.assertAlmostEqual(pack_opt.product_qty, 6.0)
+
+        self.assertAlmostEqual(move_line.qty_done, 0.0)
+        self.assertAlmostEqual(move_line.product_qty, 6.0)
+        self.assertAlmostEqual(move_line.product_uom_qty, 6.0)
+        self.assertAlmostEqual(move_line.ordered_qty, 6.0)
+
+        self.assertAlmostEqual(new_picking.move_lines.quantity_done, 0.0)
+        self.assertAlmostEqual(new_picking.move_lines.product_qty, 6.0)
+        self.assertAlmostEqual(new_picking.move_lines.product_uom_qty, 6.0)
+        self.assertAlmostEqual(new_picking.move_lines.ordered_qty, 6.0)
+
         self.assertEqual(new_picking.state, 'assigned')