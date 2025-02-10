acsone/stock-logistics-workflow

[IMP] stock_split_picking: Use specific exceptions

stock_split_picking/exceptions.py
@@ -1,0 +1,33 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _
+from odoo.exceptions import UserError
+
+
+class StockSplitPickingStateError(UserError):
+    """
+    Exception class to represent stock picking split error for picking wrong state
+    """
+
+    def __init__(self, env, picking):
+        self.env = env
+        super().__init__(
+            _(
+                "Cannot split picking %(name)s in state %(state)s",
+                name=picking.name,
+                state=picking.state,
+            )
+        )
+
+
+class StockSplitPickingError(UserError):
+    """
+    Exception class to represent stock picking split error for picking
+    """
+
+    def __init__(self, env, picking):
+        self.env = env
+        super().__init__(
+            _("Cannot split off all moves from picking %(name)s", name=picking.name)
+        )

stock_split_picking/models/stock_picking.py
@@ -6,6 +6,8 @@ from odoo import _, models
 from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_compare
 
+from ..exceptions import StockSplitPickingError, StockSplitPickingStateError
+
 
 class StockPicking(models.Model):
     """Adds picking split without done state."""
@@ -100,16 +102,10 @@ class StockPicking(models.Model):
         new_picking = self.env["stock.picking"]
         for this in self:
             if this.state in ("done", "cancel"):
-                raise UserError(
-                    _("Cannot split picking {name} in state {state}").format(
-                        name=this.name, state=this.state
-                    )
-                )
+                raise StockSplitPickingStateError(self.env, this)
             new_picking = new_picking or this._create_split_backorder()
             if not this.move_ids - moves:
-                raise UserError(
-                    _("Cannot split off all moves from picking %s") % this.name
-                )
+                raise StockSplitPickingError(self.env, this)
         moves.write({"picking_id": new_picking.id})
         moves.mapped("move_line_ids").write({"picking_id": new_picking.id})
         return new_picking

adhoc-cicd/oca-stock-logistics-workflow

stock_split_picking/exceptions.py
@@ -1,0 +1,33 @@
+# Copyright 2024 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _
+from odoo.exceptions import UserError
+
+
+class SplitPickNotAllowedInStateError(UserError):
+    """
+    Exception class to represent stock picking split error for picking wrong state
+    """
+
+    def __init__(self, env, picking):
+        self.env = env
+        super().__init__(
+            _(
+                "Cannot split picking %(name)s in state %(state)s",
+                name=picking.name,
+                state=picking.state,
+            )
+        )
+
+
+class NotPossibleToSplitPickError(UserError):
+    """
+    Exception class to represent stock picking split error for picking
+    """
+
+    def __init__(self, env, picking):
+        self.env = env
+        super().__init__(
+            _("Cannot split off all moves from picking %(name)s", name=picking.name)
+        )

stock_split_picking/models/stock_picking.py
@@ -6,6 +6,8 @@ from odoo import _, models
 from odoo.exceptions import UserError
 from odoo.tools.float_utils import float_compare
 
+from ..exceptions import NotPossibleToSplitPickError, SplitPickNotAllowedInStateError
+
 
 class StockPicking(models.Model):
     """Adds picking split without done state."""
@@ -100,16 +102,10 @@ class StockPicking(models.Model):
         new_picking = self.env["stock.picking"]
         for this in self:
             if this.state in ("done", "cancel"):
-                raise UserError(
-                    _("Cannot split picking {name} in state {state}").format(
-                        name=this.name, state=this.state
-                    )
-                )
+                raise SplitPickNotAllowedInStateError(self.env, this)
             new_picking = new_picking or this._create_split_backorder()
             if not this.move_ids - moves:
-                raise UserError(
-                    _("Cannot split off all moves from picking %s") % this.name
-                )
+                raise NotPossibleToSplitPickError(self.env, this)
         moves.write({"picking_id": new_picking.id})
         moves.mapped("move_line_ids").write({"picking_id": new_picking.id})
         return new_picking